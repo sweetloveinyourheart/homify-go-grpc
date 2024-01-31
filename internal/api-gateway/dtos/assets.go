59142d16c16c3372823e36bc3081c93444777922
nguyenlam <sweetloveinyourheart@gmail.com>
2024-01-31 23:37:49 +0700

    Add description for asset DTO

--- internal/api-gateway/dtos/assets.go
+++ internal/api-gateway/dtos/assets.go
@@ -1,11 +1,13 @@
 package dtos
 
+// NewAssets represents the data required for create new asset
 type NewAssets struct {
 	AssetType string `validate:"required,oneof=category amenity" json:"asset_type"`
 	Name      string `validate:"required"`
 	IconURL   string `validate:"required,url" json:"icon_url"`
 }
 
+// ModifyAssets represents the data required for update the assets information
 type ModifyAssets struct {
 	AssetType string `validate:"required,oneof=category amenity" json:"asset_type"`
 	Name      string `validate:"omitempty"`
